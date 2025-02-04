e5524a7520f57cad93ad227e2619aee5a2b777d4
Sebastian Salazar <sebastian.salazar.solano@gmail.com>
2025-02-04 11:41:05 -0600

    feat: add shadcn, downgrade to react 18 and add shadcn component

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,13 @@
+'use client'
+
+import { Calendar } from "@/components/ui/calendar"
+import { cn } from "@/lib/utils"
 import Image from "next/image";
+import React from "react";
+
 
 export default function Home() {
+  const [date, setDate] = React.useState<Date | undefined>(new Date())
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
@@ -11,6 +18,12 @@ export default function Home() {
           width={180}
           height={38}
           priority
+        />
+        <Calendar
+          mode="single"
+          selected={date}
+          onSelect={setDate}
+          className="rounded-md border"
         />
         <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
           <li className="mb-2">
